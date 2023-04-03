1b195e92b94e388ee949ee8792adbf7626f13425
Leonard Pak <leopak2000@gmail.com>
2023-04-04 02:48:21 +0300

    remake to haproxy

--- configs/balancer.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "
--A -t balancer:80 -s rr
--a -t balancer:80 -r load-balance-backend-1:8000 -m
--a -t balancer:80 -r load-balance-backend-2:8000 -m
--a -t balancer:80 -r load-balance-backend-3:8000 -m
-" | ipvsadm -R
-sleep infinity
